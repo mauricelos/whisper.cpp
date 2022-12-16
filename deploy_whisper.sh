ab0258d91f609e594ea4197c589f3e32f716639e
Maurice Loskyll <maurice@loskyll.de>
2022-12-16 10:58:40 +0100

    Use optimized binary for deployment

--- deploy_whisper.sh
+++ deploy_whisper.sh
@@ -1,5 +1,5 @@
 yes | rm -r -f deployment
 mkdir -p deployment/
-cp "$(python bazelisk/bazelisk.py info bazel-genfiles)/whisper.exe" deployment/
+cp "$(python bazelisk/bazelisk.py info --compilation_mode=opt bazel-bin)/whisper.exe" deployment/
 cp models/ggml-tiny.en.bin deployment/
 cp samples/samples_jfk.wav deployment/
