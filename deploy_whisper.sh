7b65152be81392d24834912a3744c663ee481de8
Maurice Loskyll <maurice@loskyll.de>
2022-12-16 16:12:37 +0100

    Fix zipped contents

--- deploy_whisper.sh
+++ deploy_whisper.sh
@@ -5,4 +5,6 @@ mkdir -p deployment/
 cp "$(python3 bazelisk/bazelisk.py info --compilation_mode=opt bazel-bin)/whisper" deployment/
 cp models/ggml-tiny.en.bin deployment/
 cp samples/samples_jfk.wav deployment/
-zip -r deployment/whisper.zip deployment
+cd deployment
+zip -r whisper.zip *
+cd ..
