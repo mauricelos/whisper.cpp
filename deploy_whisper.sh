9d4c7b81ad0e766c85feed7079ad46a29e980c51
Maurice Loskyll <maurice@loskyll.de>
2022-12-16 15:46:30 +0100

    Add release pipeline

--- deploy_whisper.sh
+++ deploy_whisper.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-"yes || true" | rm -r -f deployment
+rm -r -f deployment
 mkdir -p deployment/
 cp "$(python3 bazelisk/bazelisk.py info --compilation_mode=opt bazel-bin)/whisper" deployment/
 cp models/ggml-tiny.en.bin deployment/
 cp samples/samples_jfk.wav deployment/
+zip -r deployment/whisper.zip deployment
